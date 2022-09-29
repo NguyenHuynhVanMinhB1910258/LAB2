6af2ea56be68f4776bd54419ab200afee1cfac7f
kaijmas <kaijmas2000@gmail.com>
2022-09-29 14:06:46 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/utils/mongodb.util.js
@@ -0,0 +1,10 @@
+// const { MongodbClient } = require ("mongodb");
+var MongodbClient = require('mongodb').MongoClient; 
+class MongoDB {
+    static connect = async (uri) => {
+        if (this.client) return this.client;
+        this.client = await MongodbClient.connect(uri);
+        return this.client;
+    };
+}
+module.exports = MongoDB;
